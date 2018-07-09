d2ae9aa66e243323f86092730dc4304cb4bfd1e7
oyvindhermansen <oyvind@ludensreklame.no>
2018-07-09 22:52:00 +0200

    added another jest snapshot

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -3,14 +3,30 @@ import { create } from 'react-test-renderer';
 import Layout from '../src/index';
 
 describe('Layout', () => {
-  it('renders the simplest example of the Layout', () => {
-    const layoutComponent = (
+  test('Simplest example of the Layout', () => {
+    const layout = (
       <Layout container>
         <Layout item>hello world</Layout>
       </Layout>
     );
 
-    const tree = create(layoutComponent).toJSON();
+    const tree = create(layout).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Simple two column layout', () => {
+    const layout = (
+      <Layout container gap={16}>
+        <Layout item lg={6}>
+          I'm positioned left
+        </Layout>
+        <Layout item lg={6}>
+          I'm positioned right
+        </Layout>
+      </Layout>
+    );
+
+    const tree = create(layout).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
