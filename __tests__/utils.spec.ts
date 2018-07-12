66db59b10500b1353a8c17c721930bcece00d933
oyvindhermansen <oyvind@ludensreklame.no>
2018-07-12 16:56:31 +0200

    oh my god, it wont work because of this stupid object smerge

--- __tests__/utils.spec.ts
+++ __tests__/utils.spec.ts
@@ -2,17 +2,17 @@ import { gridCalc } from '../src/utils';
 
 describe('Utils', () => {
   test('gridCalc to calculate correct percentage based on given size', () => {
-    expect(gridCalc(3)).toEqual(25);
-    expect(gridCalc(6)).toEqual(50);
-    expect(gridCalc(12)).toEqual(100);
+    expect(gridCalc(12, 3)).toEqual(25);
+    expect(gridCalc(12, 6)).toEqual(50);
+    expect(gridCalc(12, 12)).toEqual(100);
   });
 
   test('gridCalc to throw when size is 0 or above 12', () => {
     expect(() => {
-      gridCalc(13);
+      gridCalc(12, 13);
     }).toThrow();
     expect(() => {
-      gridCalc(0);
+      gridCalc(12, 0);
     }).toThrow();
   });
 });
